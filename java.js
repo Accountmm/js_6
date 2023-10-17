3feca111d14f759ebc63ca0eb315ab2f4bdca05d
mm <proweb2008.m@gmail.com>
2023-10-17 16:06:36 +0500

    done

---
+++ java.js
@@ -0,0 +1,10 @@
+let a = + prompt ("1 soni kirirting ")
+
+while (isNaN(a) || a == 0) {
+    a=+ prompt("something is wrong "+ " you have a chance")
+}
+if (a % 2 == 0 ) {
+    alert("juft ")
+}else if (a % 2 > 0 ) {
+    alert ("tok")
+}
